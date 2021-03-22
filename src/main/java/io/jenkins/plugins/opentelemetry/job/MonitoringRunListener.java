134482faa38d4b310e6a102626a5832fc648837f
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-03-22 16:40:17 +0000

    Add build description attribute

--- src/main/java/io/jenkins/plugins/opentelemetry/job/MonitoringRunListener.java
+++ src/main/java/io/jenkins/plugins/opentelemetry/job/MonitoringRunListener.java
@@ -87,7 +87,7 @@ public class MonitoringRunListener extends OtelContextAwareAbstractRunListener {
         String rootSpanName = run.getParent().getFullName();
         String runUrl = Objects.toString(Jenkins.get().getRootUrl(), "") + run.getUrl();
         SpanBuilder rootSpanBuilder = getTracer().spanBuilder(rootSpanName)
-                .setAttribute(JenkinsOtelSemanticAttributes.CI_PIPELINE_ID, run.getParent().getFullName())
+                .setAttribute(JenkinsOtelSemanticAttributes.CI_PIPELINE_ID, rootSpanName)
                 .setAttribute(JenkinsOtelSemanticAttributes.CI_PIPELINE_NAME, run.getParent().getFullDisplayName())
                 .setAttribute(JenkinsOtelSemanticAttributes.CI_PIPELINE_RUN_URL, runUrl)
                 .setAttribute(JenkinsOtelSemanticAttributes.CI_PIPELINE_RUN_NUMBER, (long) run.getNumber())
@@ -189,7 +189,9 @@ public class MonitoringRunListener extends OtelContextAwareAbstractRunListener {
             if (runResult == null) {
                 parentSpan.setStatus(StatusCode.UNSET);
             } else {
+                String description = run.getDescription();
                 parentSpan.setAttribute(JenkinsOtelSemanticAttributes.CI_PIPELINE_RUN_COMPLETED, runResult.completeBuild);
+                parentSpan.setAttribute(JenkinsOtelSemanticAttributes.CI_PIPELINE_RUN_DESCRIPTION, description);
                 parentSpan.setAttribute(JenkinsOtelSemanticAttributes.CI_PIPELINE_RUN_RESULT, runResult.toString());
                 StatusCode statusCode = Result.SUCCESS.equals(runResult) ? StatusCode.OK : StatusCode.ERROR;
                 parentSpan.setStatus(statusCode);
--- src/test/java/io/jenkins/plugins/opentelemetry/JenkinsOtelPluginIntegrationTest.java
+++ src/test/java/io/jenkins/plugins/opentelemetry/JenkinsOtelPluginIntegrationTest.java
@@ -379,6 +379,34 @@ public class JenkinsOtelPluginIntegrationTest {
 
     }
 
+	@Test
+	public void testMultibranchPipelineStep() throws Exception {
+		String pipelineScript = "def xsh(cmd) {if (isUnix()) {sh cmd} else {bat cmd}};\n" +
+				"node() {\n" +
+				"    stage('ze-parallel-stage') {\n" +
+				"        parallel parallelBranch1: {\n" +
+				"            xsh (label: 'shell-1', script: 'echo this-is-the-parallel-branch-1')\n" +
+				"        } ,parallelBranch2: {\n" +
+				"            xsh (label: 'shell-2', script: 'echo this-is-the-parallel-branch-2')\n" +
+				"        } ,parallelBranch3: {\n" +
+				"            xsh (label: 'shell-3', script: 'echo this-is-the-parallel-branch-3')\n" +
+				"        }\n" +
+				"    }\n" +
+				"}";
+		final Node node = jenkinsRule.createOnlineSlave();
+
+		WorkflowJob pipeline = jenkinsRule.createProject(WorkflowJob.class, "test-pipeline-with-parallel-step" + jobNameSuffix.incrementAndGet());
+		pipeline.setDefinition(new CpsFlowDefinition(pipelineScript, true));
+		WorkflowRun build = jenkinsRule.assertBuildStatus(Result.SUCCESS, pipeline.scheduleBuild2(0));
+
+		final Tree<SpanDataWrapper> spans = getGeneratedSpans();
+		checkChainOfSpans(spans, "shell-1", "Parallel branch: parallelBranch1", "Stage: ze-parallel-stage", "Node", "Phase: Run");
+		checkChainOfSpans(spans, "shell-2", "Parallel branch: parallelBranch2", "Stage: ze-parallel-stage", "Node", "Phase: Run");
+		checkChainOfSpans(spans, "shell-3", "Parallel branch: parallelBranch3", "Stage: ze-parallel-stage", "Node", "Phase: Run");
+		MatcherAssert.assertThat(spans.cardinality(), CoreMatchers.is(13L));
+
+	}
+
     protected Tree<SpanDataWrapper> getGeneratedSpans() {
 
         CompletableResultCode completableResultCode = this.openTelemetrySdkProvider.getOpenTelemetrySdk().getSdkTracerProvider().forceFlush();
