b36fa12d8a789a3044867ae205a433fc137fd7fc
Cyrille Le Clerc <cyrille.leclerc@elastic.co>
2021-05-21 16:03:35 +0200

    Clearer lifecycle handling

--- src/main/java/io/jenkins/plugins/opentelemetry/init/OpenTelemetryPluginAbstractInitializer.java
+++ src/main/java/io/jenkins/plugins/opentelemetry/init/OpenTelemetryPluginAbstractInitializer.java
@@ -6,6 +6,7 @@
 package io.jenkins.plugins.opentelemetry.init;
 
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.jenkins.plugins.opentelemetry.OpenTelemetrySdkProvider;
 
 import javax.annotation.Nonnull;
@@ -13,8 +14,9 @@ import javax.inject.Inject;
 
 public abstract class OpenTelemetryPluginAbstractInitializer {
 
+    @SuppressFBWarnings(value = "URF_UNREAD_FIELD", justification = "will be used once `GlobalMeterProvider#getMeter(String)` is replaced by a getter on `OpenTelemetrySdk`")
     private OpenTelemetrySdkProvider openTelemetrySdkProvider;
-    
+
     /**
      * WARNING do not remove this setter used to surface the dependency to first initialize the OpenTelemetry SDK and then register metrics.
      * Note that once {@link io.opentelemetry.api.metrics.GlobalMeterProvider#getMeter(String)} is replaced by a getter on {@link io.opentelemetry.api.OpenTelemetry},
