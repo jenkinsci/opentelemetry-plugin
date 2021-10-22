41fd0126b977661799a2c6b6e83e9db52a699f21
Cyrille Le Clerc <cyrille.leclerc@elastic.co>
2021-10-22 16:27:57 +0200

    Add support for Jaeger and Prometheus exporters via Otel SDK Auto Configuration Extension

--- src/main/java/io/jenkins/plugins/opentelemetry/OpenTelemetryConfiguration.java
+++ src/main/java/io/jenkins/plugins/opentelemetry/OpenTelemetryConfiguration.java
@@ -5,15 +5,26 @@
 
 package io.jenkins.plugins.opentelemetry;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.jenkins.plugins.opentelemetry.authentication.NoAuthentication;
 import io.jenkins.plugins.opentelemetry.authentication.OtlpAuthentication;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+import org.apache.commons.lang.StringUtils;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class OpenTelemetryConfiguration {
+
+    @SuppressFBWarnings
+    @VisibleForTesting
+    public static boolean TESTING_INMEMORY_MODE = false;
 
     private final String endpoint;
     private final String trustedCertificatesPem;
@@ -79,6 +90,49 @@ public class OpenTelemetryConfiguration {
         return exporterIntervalMillis;
     }
 
+    public Map<String, String> toOpenTelemetryAutoConfigurationProperties() {
+        Map<String, String> configuration = new HashMap<>();
+        Map<String, String> resourceAttributes = new HashMap<>();
+
+        if (StringUtils.isNotBlank(endpoint)){
+            Preconditions.checkArgument(
+                endpoint.startsWith("http://") ||
+                    endpoint.startsWith("https://"),
+                "endpoint must be prefixed by 'http://' or 'https://': %s", endpoint);
+
+            configuration.put("otel.traces.exporter", "otlp");
+            configuration.put("otel.metrics.exporter", "otlp");
+            configuration.put("otel.exporter.otlp.endpoint", endpoint);
+        }
+        if (StringUtils.isNotBlank(serviceName)) {
+            configuration.put("otel.service.name", serviceName);
+        }
+        if (StringUtils.isNotBlank(serviceNamespace)) {
+            resourceAttributes.put(ResourceAttributes.SERVICE_NAMESPACE.getKey(), serviceNamespace);
+        }
+        if (StringUtils.isNotBlank(trustedCertificatesPem)) {
+            configuration.put("otel.exporter.otlp.certificate", trustedCertificatesPem);
+        }
+        if (authentication != null) {
+            authentication.enrichOpenTelemetryAutoConfigureConfigProperties(configuration);
+        }
+        if (exporterTimeoutMillis != null) {
+            configuration.put("otel.exporter.otlp.timeout", Integer.toString(exporterTimeoutMillis));
+        }
+        if (exporterIntervalMillis != null) {
+            configuration.put("otel.imr.export.interval", Integer.toString(exporterIntervalMillis));
+        }
+
+        configuration.put("otel.resource.attributes", OtelUtils.getComaSeparatedString(resourceAttributes));
+
+        if (OpenTelemetryConfiguration.TESTING_INMEMORY_MODE) {
+            configuration.put("otel.traces.exporter", "testing");
+            configuration.put("otel.metrics.exporter", "testing");
+            configuration.put("otel.imr.export.interval", "10");
+        }
+
+        return  configuration;
+    }
     @Nullable
     public String getIgnoredSteps() {
         return ignoredSteps;
