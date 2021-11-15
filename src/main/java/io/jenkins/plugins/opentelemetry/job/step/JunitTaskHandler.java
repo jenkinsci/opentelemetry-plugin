1363e38f4b3d6614207b9c75286822db858e245d
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-11-15 18:22:35 +0000

    Use new method

--- src/main/java/io/jenkins/plugins/opentelemetry/job/step/JunitTaskHandler.java
+++ src/main/java/io/jenkins/plugins/opentelemetry/job/step/JunitTaskHandler.java
@@ -3,13 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.jenkins.plugins.opentelemetry.job;
+package io.jenkins.plugins.opentelemetry.job.step;
 
 import hudson.Extension;
 import hudson.tasks.junit.pipeline.JUnitResultsStep;
+import io.jenkins.plugins.opentelemetry.job.JunitAction;
 import io.opentelemetry.api.common.AttributeKey;
-import io.opentelemetry.api.trace.SpanBuilder;
-import io.opentelemetry.api.trace.Tracer;
 import jenkins.YesNoMaybe;
 import org.jenkinsci.plugins.workflow.cps.nodes.StepAtomNode;
 import org.jenkinsci.plugins.workflow.graph.FlowNode;
@@ -23,15 +22,14 @@ import java.util.Map;
  * Customization of spans for {@code junit} step.
  */
 @Extension(optional = true, dynamicLoadable = YesNoMaybe.YES)
-public class JunitTaskHandler implements StepHandler {
+public class JunitTaskHandler extends DefaultStepHandler {
     @Override
     public boolean canCreateSpanBuilder(@Nonnull FlowNode flowNode, @Nonnull WorkflowRun run) {
         return flowNode instanceof StepAtomNode && ((StepAtomNode) flowNode).getDescriptor() instanceof JUnitResultsStep.DescriptorImpl;
     }
 
-    @Nonnull
     @Override
-    public SpanBuilder createSpanBuilder(@Nonnull FlowNode node, @Nonnull  WorkflowRun run, @Nonnull Tracer tracer) throws Exception {
+    public void afterSpanCreated(StepAtomNode node, WorkflowRun run) {
         JunitAction junitAction = new JunitAction();
         Map<String, String> attributes = new HashMap<>();
         attributes.put("foo", "bar");
@@ -39,6 +37,5 @@ public class JunitTaskHandler implements StepHandler {
             junitAction.getAttributes().put(AttributeKey.stringKey(attribute.getKey()), attribute.getValue());
         }
         run.addAction(junitAction);
-        return tracer.spanBuilder(node.getDisplayFunctionName());
     }
 }
