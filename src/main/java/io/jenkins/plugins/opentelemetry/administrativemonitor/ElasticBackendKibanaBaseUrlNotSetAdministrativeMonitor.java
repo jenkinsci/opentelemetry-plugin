5a293fbf87233aa054a383c70ce1b9343fbf72bd
Cyrille Le Clerc <cyrille.leclerc@elastic.co>
2021-12-12 23:37:08 +0100

    Add administrative monitors to indicate when the config is not complete

---
+++ src/main/java/io/jenkins/plugins/opentelemetry/administrativemonitor/ElasticBackendKibanaBaseUrlNotSetAdministrativeMonitor.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright The Original Author or Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.jenkins.plugins.opentelemetry.administrativemonitor;
+
+import hudson.Extension;
+import hudson.model.AdministrativeMonitor;
+import io.jenkins.plugins.opentelemetry.JenkinsOpenTelemetryPluginConfiguration;
+import io.jenkins.plugins.opentelemetry.backend.ElasticBackend;
+import org.kohsuke.stapler.HttpResponse;
+import org.kohsuke.stapler.HttpResponses;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.stapler.interceptor.RequirePOST;
+import org.parboiled.common.StringUtils;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+@Extension
+public class ElasticBackendKibanaBaseUrlNotSetAdministrativeMonitor extends AdministrativeMonitor {
+
+    private JenkinsOpenTelemetryPluginConfiguration pluginConfiguration;
+
+    @Override
+    public String getDisplayName() {
+        return "OpenTelemetry - Elastic - Kibana base URL not set";
+    }
+
+    @Override
+    public boolean isActivated() {
+        return pluginConfiguration.getObservabilityBackends().stream().
+            filter(backend -> backend instanceof ElasticBackend).
+            map(backend -> (ElasticBackend) backend).
+            filter( backend -> StringUtils.isEmpty(backend.getKibanaBaseUrl())).
+            count() > 0;
+    }
+
+    /**
+     * Depending on whether the user said "yes" or "no", send him to the right place.
+     */
+    @RequirePOST
+    public HttpResponse doAct(StaplerRequest req, StaplerResponse rsp) throws IOException {
+        if (req.hasParameter("no")) {
+            disable(true);
+            return HttpResponses.redirectToDot();
+        } else {
+            return HttpResponses.redirectViaContextPath("/configure");
+        }
+    }
+
+
+    @Inject
+    public void setJenkinsOpenTelemetryPluginConfiguration(JenkinsOpenTelemetryPluginConfiguration jenkinsOpenTelemetryPluginConfiguration) {
+        this.pluginConfiguration = jenkinsOpenTelemetryPluginConfiguration;
+    }
+}
