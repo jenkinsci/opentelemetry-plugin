65ea92d002d1623fffe3d1a941af338a9fdbe6a4
Cyrille Le Clerc <cyrille.leclerc@elastic.co>
2022-02-10 09:28:06 +0100

    extract integration tests configuration from code

--- src/test/java/io/jenkins/plugins/opentelemetry/backend/elastic/ElasticsearchRetrieverIT.java
+++ src/test/java/io/jenkins/plugins/opentelemetry/backend/elastic/ElasticsearchRetrieverIT.java
@@ -8,17 +8,26 @@ package io.jenkins.plugins.opentelemetry.backend.elastic;
 import io.jenkins.plugins.opentelemetry.job.log.LogsQueryResult;
 import org.apache.http.auth.Credentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
 
 public class ElasticsearchRetrieverIT {
 
     @Test
     public void test() throws IOException {
-        String url = "https://my-deployment-76ea19.es.europe-west1.gcp.cloud.es.io:9243";
-        String username = "jenkins";
-        String password = "W0&7282TMPgS4Qk";
+        InputStream envAsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(".env");
+        Assert.assertTrue(".env file not found in classpath", envAsStream != null);
+        Properties env = new Properties();
+        env.load(envAsStream);
+
+        String url = env.getProperty("elasticsearch.url"); ;
+        String username = env.getProperty("elasticsearch.username"); ;
+        String password = env.getProperty("elasticsearch.password"); ;
         String indexPattern = "logs-apm.app-*";
 
         ElasticsearchLogStorageRetriever elasticsearchLogStorageRetriever = new ElasticsearchLogStorageRetriever(
