313b10ce347023adcec6b85cf864e635cb3dc7a0
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-02-24 19:08:09 +0000

    Add exclusive logs jcasc

---
+++ src/test/java/io/jenkins/plugins/opentelemetry/jcasc/ConfigurationAsCodeElasticLogsBackendExclusiveTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright The Original Author or Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.jenkins.plugins.opentelemetry.jcasc;
+
+import io.jenkins.plugins.casc.ConfigurationContext;
+import io.jenkins.plugins.casc.ConfiguratorRegistry;
+import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import io.jenkins.plugins.casc.model.CNode;
+import io.jenkins.plugins.opentelemetry.JenkinsOpenTelemetryPluginConfiguration;
+import io.jenkins.plugins.opentelemetry.authentication.NoAuthentication;
+import io.jenkins.plugins.opentelemetry.authentication.OtlpAuthentication;
+import io.jenkins.plugins.opentelemetry.backend.ElasticBackend;
+import io.jenkins.plugins.opentelemetry.backend.elastic.ElasticLogsBackendWithoutJenkinsVisualization;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import jenkins.model.GlobalConfiguration;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import static io.jenkins.plugins.casc.misc.Util.getUnclassifiedRoot;
+import static io.jenkins.plugins.casc.misc.Util.toStringFromYamlFile;
+import static io.jenkins.plugins.casc.misc.Util.toYamlString;
+import static org.hamcrest.CoreMatchers.instanceOf;
+
+public class ConfigurationAsCodeElasticLogsBackendExclusiveTest {
+
+    @ClassRule
+    @ConfiguredWithCode("elastic-logs-exclusive.yml")
+    public static JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
+
+    @Test
+    public void should_support_configuration_as_code() {
+        final JenkinsOpenTelemetryPluginConfiguration configuration = GlobalConfiguration.all().get(JenkinsOpenTelemetryPluginConfiguration.class);
+
+        MatcherAssert.assertThat(configuration.getEndpoint(), CoreMatchers.is("http://otel-collector-contrib:4317"));
+
+        ElasticBackend elastic = (ElasticBackend) configuration.getObservabilityBackends().get(0);
+        MatcherAssert.assertThat(elastic.getKibanaBaseUrl(), CoreMatchers.is("https://kibana.europe-west1.gcp.cloud.es.io:9243"));
+        MatcherAssert.assertThat(elastic.getName(), CoreMatchers.is("My Elastic"));
+
+        ElasticLogsBackendWithoutJenkinsVisualization elasticLogsBackend = (ElasticLogsBackendWithoutJenkinsVisualization) elastic.getElasticLogsBackend();
+        MatcherAssert.assertThat(elasticLogsBackend, CoreMatchers.is(CoreMatchers.notNullValue()));
+
+        OtlpAuthentication authentication = configuration.getAuthentication();
+        MatcherAssert.assertThat(authentication, CoreMatchers.is(instanceOf(NoAuthentication.class)));
+
+        MatcherAssert.assertThat(configuration.getExporterTimeoutMillis(), CoreMatchers.nullValue());
+        MatcherAssert.assertThat(configuration.getExporterIntervalMillis(), CoreMatchers.nullValue());
+
+        MatcherAssert.assertThat(configuration.getIgnoredSteps(), CoreMatchers.is("dir,echo,isUnix,pwd,properties"));
+
+        MatcherAssert.assertThat(configuration.getServiceName(), CoreMatchers.is("jenkins"));
+        MatcherAssert.assertThat(configuration.getServiceNamespace(), CoreMatchers.is("jenkins"));
+    }
+
+    @Test
+    public void should_support_configuration_export() throws Exception {
+        ConfiguratorRegistry registry = ConfiguratorRegistry.get();
+        ConfigurationContext context = new ConfigurationContext(registry);
+        CNode yourAttribute = getUnclassifiedRoot(context).get("openTelemetry");
+
+        String exported = toYamlString(yourAttribute);
+
+        String expected = toStringFromYamlFile(this, "elastic-logs-exclusive-expected.yml");
+
+        MatcherAssert.assertThat(exported, CoreMatchers.is(expected));
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        GlobalOpenTelemetry.resetForTest();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        GlobalOpenTelemetry.resetForTest();
+    }
+}
