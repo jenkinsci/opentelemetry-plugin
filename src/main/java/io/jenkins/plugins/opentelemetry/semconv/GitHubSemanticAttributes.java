bd2bbcdd2cca48cb134270b6c02992384301470e
Cyrille Le Clerc <cyrille.leclerc@elastic.co>
2022-04-27 15:12:05 +0200

    add `github.app.name` attribute rather than `jenkins.credentials.id` to characterize the GitHub API Rate Limit

--- src/main/java/io/jenkins/plugins/opentelemetry/semconv/GitHubSemanticAttributes.java
+++ src/main/java/io/jenkins/plugins/opentelemetry/semconv/GitHubSemanticAttributes.java
@@ -10,6 +10,8 @@ import io.opentelemetry.api.common.AttributeKey;
 public final class GitHubSemanticAttributes {
 
     public static final AttributeKey<String> GITHUB_APP_ID = AttributeKey.stringKey("github.app.id");
+
+    public static final AttributeKey<String> GITHUB_APP_NAME = AttributeKey.stringKey("github.app.name");
     public static final AttributeKey<String> GITHUB_APP_OWNER = AttributeKey.stringKey("github.app.owner");
     public static final AttributeKey<String> GITHUB_AUTHENTICATION = AttributeKey.stringKey("github.authentication");
     public static final AttributeKey<String> GITHUB_API_URL = AttributeKey.stringKey("github.api.url");
