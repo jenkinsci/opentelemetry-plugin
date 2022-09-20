f8aedb2941b398d9828007e5e4e670054c54cb38
Cyrille Le Clerc <cyrille.leclerc@elastic.co>
2022-09-20 18:56:28 +0200

    Fix support for multiple OpenTelemetry SDK metrics exporters

--- src/main/java/io/jenkins/plugins/opentelemetry/OpenTelemetryConfiguration.java
+++ src/main/java/io/jenkins/plugins/opentelemetry/OpenTelemetryConfiguration.java
@@ -16,7 +16,6 @@ import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import org.apache.commons.lang.StringUtils;
 
 import javax.annotation.Nonnull;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
---
+++ src/test/java/io/jenkins/plugins/opentelemetry/OpenTelemetryConfigurationTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright The Original Author or Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.jenkins.plugins.opentelemetry;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class OpenTelemetryConfigurationTest {
+
+    @Test
+    public void testSingleOpenTelemetryExporters() {
+        String expectedTracesExporter = "otlp";
+        String expectedMetricsExporter = "otlp";
+        String expectedLogsExporter = null;
+        // OTLP endpoint configured through the GUI
+        String otlpEndpoint = "http://localhost:4317";
+
+        Map<String, String> configurationProperties = new HashMap<>();
+        testOpenTelemetryExportersConfiguration(expectedTracesExporter, expectedMetricsExporter, expectedLogsExporter, otlpEndpoint, configurationProperties);
+    }
+
+    @Test
+    public void testMultipleOpenTelemetryExporters_1() {
+        String expectedTracesExporter = "otlp";
+        String expectedMetricsExporter = "otlp,prometheus";
+        String expectedLogsExporter = null;
+        // OTLP endpoint configured through the GUI
+        String otlpEndpoint = "http://localhost:4317";
+
+        Map<String, String> configurationProperties = Collections.singletonMap("otel.metrics.exporter", "otlp,prometheus");
+        testOpenTelemetryExportersConfiguration(expectedTracesExporter, expectedMetricsExporter, expectedLogsExporter, otlpEndpoint, configurationProperties);
+    }
+
+    @Test
+    public void testMultipleOpenTelemetryExporters_2() {
+        String expectedTracesExporter = "otlp";
+        String expectedMetricsExporter = "prometheus,otlp";
+        String expectedLogsExporter = null;
+        // OTLP endpoint configured through the GUI
+        String otlpEndpoint = "http://localhost:4317";
+
+        Map<String, String> configurationProperties = Collections.singletonMap("otel.metrics.exporter", "prometheus");
+        testOpenTelemetryExportersConfiguration(expectedTracesExporter, expectedMetricsExporter, expectedLogsExporter, otlpEndpoint, configurationProperties);
+    }
+
+    private static void testOpenTelemetryExportersConfiguration(String expectedTracesExporter, String expectedMetricsExporter, String expectedLogsExporter, String otlpEndpoint, Map<String, String> configurationProperties) {
+        OpenTelemetryConfiguration configuration = new OpenTelemetryConfiguration(
+            Optional.ofNullable(otlpEndpoint),
+            Optional.empty(),
+            Optional.empty(),
+            Optional.empty(),
+            Optional.empty(),
+            Optional.of("my-jenkins"),
+            Optional.empty(),
+            Optional.empty(),
+            configurationProperties);
+
+        Map<String, String> actualOtelProperties = configuration.toOpenTelemetryProperties();
+        String actualTracesExporter = actualOtelProperties.get("otel.traces.exporter");
+        String actualMetricsExporter = actualOtelProperties.get("otel.metrics.exporter");
+        String actualLogsExporter = actualOtelProperties.get("otel.logs.exporter");
+
+        MatcherAssert.assertThat(actualTracesExporter, Matchers.is(expectedTracesExporter));
+        MatcherAssert.assertThat(actualMetricsExporter, Matchers.is(expectedMetricsExporter));
+        MatcherAssert.assertThat(actualLogsExporter, Matchers.is(expectedLogsExporter));
+    }
+}
