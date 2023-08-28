e250a664a10dae32721e60bc24560f3b499ad5d5
Cyrille Le Clerc <cyrille.leclerc@grafana.com>
2023-08-28 21:21:12 +0800

    Support `none` exporter for traces and metrics
    Signed-off-by: Cyrille Le Clerc <cyrille.leclerc@grafana.com>

--- src/main/java/io/jenkins/plugins/opentelemetry/OpenTelemetryConfiguration.java
+++ src/main/java/io/jenkins/plugins/opentelemetry/OpenTelemetryConfiguration.java
@@ -103,8 +103,8 @@ public class OpenTelemetryConfiguration {
             properties.put("otel.imr.export.interval", "10ms");
         } else if (this.getEndpoint().isPresent()) {
             this.getEndpoint().ifPresent(endpoint -> { // prepare of Optional.ifPResentOrElse()
-                properties.compute("otel.traces.exporter", (key, oldValue) -> oldValue == null ? "otlp" : oldValue.contains("otlp") ? oldValue : oldValue.concat(",otlp"));
-                properties.compute("otel.metrics.exporter", (key, oldValue) -> oldValue == null ? "otlp" : oldValue.contains("otlp") ? oldValue : oldValue.concat(",otlp"));
+                properties.compute("otel.traces.exporter", (key, oldValue) -> oldValue == null ? "otlp" : "none".equals(oldValue) ? "none" : oldValue.contains("otlp") ? oldValue : oldValue.concat(",otlp"));
+                properties.compute("otel.metrics.exporter", (key, oldValue) -> oldValue == null ? "otlp" : "none".equals(oldValue) ? "none" : oldValue.contains("otlp") ? oldValue : oldValue.concat(",otlp"));
                 properties.put("otel.exporter.otlp.endpoint", endpoint);
             });
         } else if (StringUtils.isBlank(OtelUtils.getSystemPropertyOrEnvironmentVariable("OTEL_TRACES_EXPORTER")) &&
