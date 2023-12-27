9c04a5825d6c3d14caad6553dae30e4749c2fe0c
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2023-12-27 20:47:58 +0100

    fix: clean up tests

--- src/test/java/io/jenkins/plugins/opentelemetry/backend/elastic/ElasticsearchBackendITTest.java
+++ src/test/java/io/jenkins/plugins/opentelemetry/backend/elastic/ElasticsearchBackendITTest.java
@@ -25,7 +25,7 @@ import io.jenkins.plugins.opentelemetry.job.log.LogStorageRetriever;
 import org.apache.commons.io.IOUtils;
 import java.nio.charset.StandardCharsets;
 
-public class PipelineElasticsearchBackendTest extends ElasticStackIT {
+public class ElasticsearchBackendITTest extends ElasticStackIT {
 
     @Test
     public void test() throws Exception {
@@ -38,8 +38,34 @@ public class PipelineElasticsearchBackendTest extends ElasticStackIT {
         jenkinsRule.assertLogContains("Hello", run);
     }
 
+    @Test
+    public void testCredentialsDoValidate() {
+        ElasticBackend backend = elasticStack.getElasticBackendConfiguration();
+        ElasticBackend.DescriptorImpl descriptor = (ElasticBackend.DescriptorImpl)backend.getDescriptor();
+        ElasticLogsBackendWithJenkinsVisualization visualization = elasticStack.getElasticStackConfiguration();
+        ElasticLogsBackendWithJenkinsVisualization.DescriptorImpl visDescriptor = (ElasticLogsBackendWithJenkinsVisualization.DescriptorImpl)visualization.getDescriptor();
+        assertEquals(FormValidation.Kind.OK, descriptor.doCheckKibanaBaseUrl("http://kibana.example.com").kind);
+        assertEquals(FormValidation.Kind.OK, visDescriptor.doValidate(elasticStack.getEsUrl(), true, ElasticStack.CRED_ID).kind);
+        assertEquals(FormValidation.Kind.ERROR, visDescriptor.doValidate(elasticStack.getEsUrl(), true, ElasticStack.WRONG_CREDS).kind);
+        assertEquals(FormValidation.Kind.ERROR, visDescriptor.doValidate("nowhere", true, ElasticStack.CRED_ID).kind);
+    }
+
+    @Test
+    public void testDoFillCredentialsIdItems() {
+        ElasticLogsBackendWithJenkinsVisualization visualization = elasticStack.getElasticStackConfiguration();
+        ElasticLogsBackendWithJenkinsVisualization.DescriptorImpl visDescriptor = (ElasticLogsBackendWithJenkinsVisualization.DescriptorImpl)visualization.getDescriptor();
+        assertFalse(visDescriptor.doFillElasticsearchCredentialsIdItems(null, ElasticStack.CRED_ID).isEmpty());
+    }
+
+    @Test
+    public void testDoCheckCredentialsId() {
+        ElasticLogsBackendWithJenkinsVisualization visualization = elasticStack.getElasticStackConfiguration();
+        ElasticLogsBackendWithJenkinsVisualization.DescriptorImpl visDescriptor = (ElasticLogsBackendWithJenkinsVisualization.DescriptorImpl)visualization.getDescriptor();
+        assertEquals(FormValidation.Kind.OK, visDescriptor.doCheckElasticsearchCredentialsId(null, ElasticStack.CRED_ID).kind);
+        assertEquals(FormValidation.Kind.ERROR, visDescriptor.doCheckElasticsearchCredentialsId(null, "foo").kind);
+    }
+
     private void waitForLogs(WorkflowRun run) throws InterruptedException {
-        // volume of retrieved logs in bytes
         LogStorageRetriever elasticsearchRetriever = elasticStack.getElasticsearchRetriever();
         long logsLength = 0;
         MonitoringAction action = run.getAction(MonitoringAction.class);
