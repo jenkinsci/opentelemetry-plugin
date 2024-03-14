aa9775a2ef4983a47f1e1e522d799b62b3b8f85d
Christophe Kamphaus <christophe.kamphaus@jemmic.com>
2024-03-14 18:18:37 +0100

    173 Added test case for setOn parameter testSimplePipelineWithWithSpanAttributeStepSetOn
    The test pipeline sets the parameter setOn.

--- src/test/java/io/jenkins/plugins/opentelemetry/job/step/WithSpanAttributeStepTest.java
+++ src/test/java/io/jenkins/plugins/opentelemetry/job/step/WithSpanAttributeStepTest.java
@@ -81,4 +81,65 @@ public class WithSpanAttributeStepTest extends BaseIntegrationTest {
 
         MatcherAssert.assertThat(spans.cardinality(), CoreMatchers.is(8L));
     }
+
+    @Test
+    public void testSimplePipelineWithWithSpanAttributeStepSetOn() throws Exception {
+        assumeFalse(SystemUtils.IS_OS_WINDOWS);
+        // BEFORE
+
+        String pipelineScript = "def xsh(cmd) {if (isUnix()) {sh cmd} else {bat cmd}};\n" +
+            "withSpanAttribute(key: 'pipeline.type', value: 'release', target: 'PIPELINE_ROOT_SPAN')\n" +
+            "node() {\n" +
+            "    stage('build') {\n" +
+            "       withSpanAttribute(key: 'pipeline.importance', value: 'critical', target: 'PIPELINE_ROOT_SPAN', setOn: 'TARGET_AND_CHILDREN')\n" +
+            "       withSpanAttribute(key: 'stage.type', value: 'build-java-maven', target: 'CURRENT_SPAN', setOn: 'TARGET_ONLY')\n" +
+            "       withSpanAttribute(key: 'build.tool', value: 'maven', setOn: 'TARGET_AND_CHILDREN')\n" +
+            "       xsh (label: 'release-script', script: 'echo ze-echo-1') \n" +
+            "    }\n" +
+            "    stage('test') {\n" +
+            "       xsh (label: 'test-script', script: 'echo ze-echo-2') \n" +
+            "    }\n" +
+            "}";
+        jenkinsRule.createOnlineSlave();
+
+        final String jobName = "test-simple-pipeline-with-with-span-attribute-step-seton" + jobNameSuffix.incrementAndGet();
+        WorkflowJob pipeline = jenkinsRule.createProject(WorkflowJob.class, jobName);
+        pipeline.setDefinition(new CpsFlowDefinition(pipelineScript, true));
+        jenkinsRule.assertBuildStatus(Result.SUCCESS, pipeline.scheduleBuild2(0));
+
+        String rootSpanName = JenkinsOtelSemanticAttributes.CI_PIPELINE_RUN_ROOT_SPAN_NAME_PREFIX + jobName;
+
+        final Tree<SpanDataWrapper> spans = getGeneratedSpans();
+
+        checkChainOfSpans(spans, "Phase: Start", rootSpanName);
+        checkChainOfSpans(spans, JenkinsOtelSemanticAttributes.AGENT_ALLOCATION_UI, JenkinsOtelSemanticAttributes.AGENT_UI, "Phase: Run", rootSpanName);
+        checkChainOfSpans(spans, "release-script", "Stage: build", JenkinsOtelSemanticAttributes.AGENT_UI, "Phase: Run", rootSpanName);
+        checkChainOfSpans(spans, "Phase: Finalise", rootSpanName);
+
+        { // attribute 'pipeline.type' - PIPELINE_ROOT_SPAN
+            SpanData actualSpanData = spans.breadthFirstStream().filter(sdw -> rootSpanName.equals(sdw.spanData.getName())).findFirst().get().spanData;
+            String actualPipelineType = actualSpanData.getAttributes().get(AttributeKey.stringKey("pipeline.type"));
+            MatcherAssert.assertThat(actualPipelineType, CoreMatchers.is("release"));
+        }
+
+        { // attribute 'pipeline.importance' - PIPELINE_ROOT_SPAN, can be configured anywhere in the pipeline
+            SpanData actualSpanData = spans.breadthFirstStream().filter(sdw -> rootSpanName.equals(sdw.spanData.getName())).findFirst().get().spanData;
+            String actualPipelineType = actualSpanData.getAttributes().get(AttributeKey.stringKey("pipeline.importance"));
+            MatcherAssert.assertThat(actualPipelineType, CoreMatchers.is("critical"));
+        }
+
+        { // attribute 'stage.type' - CURRENT_SPAN
+            SpanData actualSpanData = spans.breadthFirstStream().filter(sdw -> "Stage: build".equals(sdw.spanData.getName())).findFirst().get().spanData;
+            String actualStageType = actualSpanData.getAttributes().get(AttributeKey.stringKey("stage.type"));
+            MatcherAssert.assertThat(actualStageType, CoreMatchers.is("build-java-maven"));
+        }
+
+        { // attribute 'build.tool' - implicitly CURRENT_SPAN
+            SpanData actualSpanData = spans.breadthFirstStream().filter(sdw -> "Stage: build".equals(sdw.spanData.getName())).findFirst().get().spanData;
+            String actualStageType = actualSpanData.getAttributes().get(AttributeKey.stringKey("build.tool"));
+            MatcherAssert.assertThat(actualStageType, CoreMatchers.is("maven"));
+        }
+
+        MatcherAssert.assertThat(spans.cardinality(), CoreMatchers.is(10L));
+    }
 }
