bc66b224c74dc290425ec6e5ddbc1ee88b94e89a
Cyrille Le Clerc <cyrille.leclerc@grafana.com>
2024-12-18 10:51:54 +0100

    Simplify code

--- src/main/java/io/jenkins/plugins/opentelemetry/OtelUtils.java
+++ src/main/java/io/jenkins/plugins/opentelemetry/OtelUtils.java
@@ -45,7 +45,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
 import java.util.function.Function;
-import java.util.logging.Logger;
 
 import static io.jenkins.plugins.opentelemetry.semconv.ConfigurationKey.*;
 
