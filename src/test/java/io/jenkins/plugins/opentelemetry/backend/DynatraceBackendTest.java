cc8e27db7ac4c8716f4b0e48a2a0468e22d2f34a
gabe-dt <gabriel.marques@dynatrace.com>
2025-06-10 17:35:03 -0300

    fix: test files naming

--- src/test/java/io/jenkins/plugins/opentelemetry/backend/DynatraceBackendTest.java
+++ src/test/java/io/jenkins/plugins/opentelemetry/backend/DynatraceBackendTest.java
@@ -13,12 +13,11 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class DynatraceClassicBackendTest {
+public class DynatraceBackendTest {
 
     @Test
     public void testGetMetricsVisualizationUrlDashboardIsSet() {
-        DynatraceClassicBackend backend = new DynatraceClassicBackend(
-                "https://{your-environment-id}.live.dynatrace.com/");
+        DynatraceBackend backend = new DynatraceBackend("https://{your-environment-id}.live.dynatrace.com/");
         backend.setDashboardId("311fa105-1f09-4005-926d-8d27bc33a717");
         Resource resource = Resource.builder().put(ServiceAttributes.SERVICE_NAME, "jenkins").build();
 
@@ -29,8 +28,7 @@ public class DynatraceClassicBackendTest {
 
     @Test
     public void testGetMetricsVisualizationUrlDashboardIsNotSet() {
-        DynatraceClassicBackend backend = new DynatraceClassicBackend(
-                "https://{your-environment-id}.live.dynatrace.com/");
+        DynatraceBackend backend = new DynatraceBackend("https://{your-environment-id}.live.dynatrace.com/");
         Resource resource = Resource.builder().put(ServiceAttributes.SERVICE_NAME, "jenkins").build();
 
         String actual = backend.getMetricsVisualizationUrl(resource);
