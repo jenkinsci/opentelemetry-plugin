eb1010f84be20d644421a1a340d1dfdfd519df63
Zenith1415 <amanraz.india@gmail.com>
2026-01-18 22:49:33 +0530

    chore: migrate ConfigurationKeyTest to JUnit 5

--- src/test/java/io/jenkins/plugins/opentelemetry/semconv/ConfigurationKeyTest.java
+++ src/test/java/io/jenkins/plugins/opentelemetry/semconv/ConfigurationKeyTest.java
@@ -1,21 +1,21 @@
 package io.jenkins.plugins.opentelemetry.semconv;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class ConfigurationKeyTest {
 
     @Test
     public void testConfigurationKey() {
-        Assert.assertEquals(
+        Assertions.assertEquals(
                 "OTEL_EXPORTER_OTLP_CERTIFICATE", ConfigurationKey.OTEL_EXPORTER_OTLP_CERTIFICATE.asEnvVar());
-        Assert.assertEquals(
+        Assertions.assertEquals(
                 "otel.exporter.otlp.certificate", ConfigurationKey.OTEL_EXPORTER_OTLP_CERTIFICATE.asProperty());
 
-        Assert.assertEquals(
+        Assertions.assertEquals(
                 "OTEL_INSTRUMENTATION_JENKINS_RUN_METRIC_DURATION_ALLOW_LIST",
                 ConfigurationKey.OTEL_INSTRUMENTATION_JENKINS_RUN_DURATION_ALLOW_LIST.asEnvVar());
-        Assert.assertEquals(
+        Assertions.assertEquals(
                 "otel.instrumentation.jenkins.run.metric.duration.allow_list",
                 ConfigurationKey.OTEL_INSTRUMENTATION_JENKINS_RUN_DURATION_ALLOW_LIST.asProperty());
     }
